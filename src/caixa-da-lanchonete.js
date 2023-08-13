834de361f7d7ba3cdb19bb11c6f1134352551268
Marcus Lara <marcus.lara@qhotmail.com>
2023-08-13 15:19:01 -0300

    inserindo primeiras regras de input

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -20,33 +20,44 @@ const pagamento = [
 
 class CaixaDaLanchonete {
 
-    verificaCompatibilidade(codigo){
-        
-        const codigosCardapio = cardapio.map((code) => {
-            return code.codigo;
-        })
+    verificaCompatibilidade(infoPedido, quantidades, codigos){
+        let validaCodigo = true;
 
-        const statusCardapio = cardapio.map((stt) => {
-            return stt.status;
-        })
+        const infoPedidoCodigo = infoPedido.map((code => {
+            return infoPedido.codigo;
+        }))
 
-       
+        for(const codigo of codigos) {
+            if(codigo === '') {
+                validaCodigo = false;
+                console.log("Não há itens no carrinho de compra!");
+                break;
+            }
+        }
 
-        let validaCodigo = false;
+        for(const pedido of infoPedido) {
+            if(pedido == undefined) {
+                validaCodigo = false;
+                console.log("Item inválido!");
+                break;
+            }
+        }
 
-        codigosCardapio.forEach((e) => {
-            if(e == codigo)
-                validaCodigo = true;
-        })
-
+        for(const quantidade of quantidades) {
+            if(quantidade === 0) {
+                validaCodigo = false;
+                console.log("Quantidade inválida!");
+                break;
+            }
+        }
 
         return validaCodigo;
     }
 
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-
-        const codigo = itens.map(item => {
+        
+        const codigos = itens.map(item => {
            return item.split(',')[0];
         });
 
@@ -54,21 +65,18 @@ class CaixaDaLanchonete {
             return Number(quantidade.split(',')[1]);
         })
 
-        const infoPedido = codigo.map((item) => {
+        const infoPedido = codigos.map((item) => {
             const itemDoCardapio = cardapio.find((c) => c.codigo === item);
             return itemDoCardapio;
         });
         
-        console.log(infoPedido);
 
-        
-
-        return this.verificaCompatibilidade(codigo) ;
+        return this.verificaCompatibilidade(infoPedido, quantidades, codigos) ;
     }
 
 }
 
-const novoPedido = new CaixaDaLanchonete().calcularValorDaCompra('debito', ['cafe,1','cafe,1']);
+const novoPedido = new CaixaDaLanchonete().calcularValorDaCompra('debito', ['cafe,1','chantily,1']);
 console.log(novoPedido);
 
 
